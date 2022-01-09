def14abf1b19937d2f086501ee97f657c03c4002
Joy Obor <oborjoyamara@gmail.com>
2022-01-09 20:35:57 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+
+  );
   runApp(const MyApp());
 }
 
@@ -15,6 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
               primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
